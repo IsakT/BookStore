22e00e8e7f1b1da205092d0be8d23c113bfc5274
SardonicFire <ericson.frida@hotmail.com>
2015-03-11 15:29:00 +0100

    added accordion controllers

--- bookStore/bookStore-front/js/controllers/genreController.js
+++ bookStore/bookStore-front/js/controllers/genreController.js
@@ -5,3 +5,32 @@ app.controller("genreController", ["$scope", function($scope){
 		$scope.mrBool = !$scope.mrBool;
 	};
 }]);
+
+app.controller("genreController2", ["$scope", function($scope){
+
+	$scope.oneAtATime = true;
+
+	$scope.groups = [
+	{
+	  title: 'Genre - 1',
+	  content: 'Description - 1'
+	},
+	{
+	  title: 'Genre - 2',
+	  content: 'Description- 2'
+	}
+	];
+
+	$scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+	$scope.addItem = function() {
+	var newItemNo = $scope.items.length + 1;
+	$scope.items.push('Item ' + newItemNo);
+	};
+
+	$scope.status = {
+	isFirstOpen: true,
+	isFirstDisabled: false
+	};
+
+}]);
