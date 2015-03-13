abc52b3792915ee55e78caceed44bb8998600b6d
SardonicFire <ericson.frida@hotmail.com>
2015-03-13 15:32:42 +0100

    lunch also

--- bookStore/bookStore-front/js/controllers/homeController.js
+++ bookStore/bookStore-front/js/controllers/homeController.js
@@ -1,3 +1,18 @@
 app.controller("homeController", ["$scope", function($scope){
+
+	$scope.top3 = [
+	{
+	  title: 'Genre - 1',
+	  description: 'Description - 1'
+	},
+	{
+	  title: 'Genre - 1',
+	  description: 'Description - 1'
+	},
+	{
+	  title: 'Genre - 1',
+	  description: 'Description - 1'
+	}
+	];
 	
 }]);
