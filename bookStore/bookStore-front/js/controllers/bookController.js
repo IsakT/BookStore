2fb4dcfb1fc8625f04826c24bbb3a2bc472a513d
niclas88 <niclas88@hotmail.se>
2015-03-14 17:34:03 +0100

    Lagt till fler funntioner i BookDetailView

--- bookStore/bookStore-front/js/controllers/bookController.js
+++ bookStore/bookStore-front/js/controllers/bookController.js
@@ -3,8 +3,58 @@ app.controller("bookController", ["$scope", function($scope){
 	$scope.readMore = function(){
 		$scope.more = !$scope.more;
 	}
+
+	$scope.editBook = function(){
+		$scope.showEditWindow = !$scope.showEditWindow;
+		$scope.editBookView = !$scope.editBookView;
+	}
+
+	// $scope.books = Book.index();
+
 	$scope.books = [
 		{"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
+		 "description":"jkasld",
+		 "author":"Yuval Noah Harari ",
+		 "genre":"Historia & arkeologi",
+		 "year":"2015",
+		 "isbn":" 9789127140394",
+		 "details":"asff"},
+		 {"title":"Sapiens : en kort historik över mänskligheten",
 		 "description":"jkasld",
 		 "author":"Yuval Noah Harari ",
 		 "genre":"Historia & arkeologi",
--- bookStore/bookStore-front/js/controllers/menuController.js
+++ bookStore/bookStore-front/js/controllers/menuController.js
@@ -1,8 +1,15 @@
-app.controller("menuController", ["$scope", "adminService", function($scope, adminService){
+app.controller("menuController", ["$scope", "$rootScope", "adminService", function($scope, $rootScope, adminService){
 	
 
 	$scope.login = function(){
-		adminService.login($scope)
+		// adminService.login($scope)
+		$scope.showAdd = !$scope.showAdd;
+		$rootScope.showEdit = !$rootScope.showEdit;
+		$rootScope.showSave = !$rootScope.showSave;
+		$rootScope.showTxtEdit = !$rootScope.showTxtEdit;
+		if($scope.showAdd){
+					console.log("Logged in as Admin");
+				};
 	};
 
 }]);
