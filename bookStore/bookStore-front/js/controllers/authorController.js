dc91acda710b379c8c6ce5597b5c529c7eb9c076
klimparn <kim.s.holmberg@gmail.com>
2015-03-17 12:47:20 +0100

    Uppdaterat authorController + authorView.

--- bookStore/bookStore-front/js/controllers/authorController.js
+++ bookStore/bookStore-front/js/controllers/authorController.js
@@ -1,3 +1,10 @@
-app.controller("authorController", ["$scope", function($scope){
+app.controller("authorController", ["$scope", "$http", function($scope, $http){
+
+		$http
+		.get("data/bookData.json")
+		.success(function(data){
+			console.log("Got dummydata", data)
+			$scope.bookData = data;
+	});
 	
 }]);
