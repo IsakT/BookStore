994a499c4d71d8960ef1f9cc73bca58539a8e000
SardonicFire <ericson.frida@hotmail.com>
2015-03-17 09:17:18 +0100

    fixat lite css, ändrat lite i dummydata för genreview, lagt in en genre på homeview som länkar till genreview

--- bookStore/bookStore-front/js/controllers/genreController.js
+++ bookStore/bookStore-front/js/controllers/genreController.js
@@ -30,7 +30,7 @@ app.controller("genreController2", ["$scope", function($scope){
 	},
 	{
 	  title: 'Romance',
-	  description: 'Jessica´s hand gently slid down Amandas back... And then they lived happily ever after, after having killed all the zombies in the universe. Their wedding ceremony was precided over by the Space Pope, who subsequenty joined them in a rather spirited threesome.'
+	  description: 'Jessie´s hand gently slid down Andy´s back... And then they lived happily ever after, after having killed all the zombies in the universe. Their wedding ceremony was precided over by the Space Pope, who subsequenty joined them in a rather spirited threesome.'
 	}
 	];
 
