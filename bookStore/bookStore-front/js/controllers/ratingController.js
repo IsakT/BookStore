6fb8d623cd85d82df367439abdffdec822b9ee62
Alexis <alexisrozas@yahoo.com>
2015-03-20 02:42:17 +0100

    Added Controller for Rating system.

---
+++ bookStore/bookStore-front/js/controllers/ratingController.js
@@ -0,0 +1,46 @@
+angular.module('Rater', [])
+  .controller('rateCtrl', function($scope, $window) {
+    $scope.rating = 5;
+    $scope.saveRatingToServer = function(rating) {
+      $window.alert('Rating selected - ' + rating);
+    };
+  })
+  .directive('dirRating', function () {
+    return {
+      restrict: 'A',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '=',
+        max: '=',
+        readonly: '@',
+        onRatingSelected: '&'
+      },
+      link: function (scope, elem, attrs) {
+
+        var updateStars = function() {
+          scope.stars = [];
+          for (var  i = 0; i < scope.max; i++) {
+            scope.stars.push({filled: i < scope.ratingValue});
+          }
+        };
+
+        scope.toggle = function(index) {
+          if (scope.readonly && scope.readonly === 'true') {
+            return;
+          }
+          scope.ratingValue = index + 1;
+          scope.onRatingSelected({rating: index + 1});
+        };
+
+        scope.$watch('ratingValue', function(newVal, oldVal) {
+          if (newVal || newVal === 0) {
+            updateStars();
+          }
+        });
+      }
+    }
+  });
